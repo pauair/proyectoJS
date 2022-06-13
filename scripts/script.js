20b846960f065484000888efe5ee78673f08aa56
Paula Airaudo <paula.airaudo@hotmail.com>
2022-06-12 23:15:56 -0300

    Cambios finales en carrito

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 class Producto {
-    constructor (id, nombre, descripcion, amargor, volAlc, precio, stock, moneda) {
+    constructor(id, nombre, descripcion, amargor, volAlc, precio, stock, moneda) {
         this.id = id
         this.nombre = nombre
         this.descripcion = descripcion
@@ -11,17 +11,14 @@ class Producto {
     }
 }
 
-//Listado de productos
-
+//Listado de productos en HTML
 const sectionProducts = document.getElementById("section-products")
-let productos = []
 
-fetch("../json/productos.json")
-.then(response => response.json())
-.then(productos => {
+let productos = fetch("../json/productos.json").then(res => res.json())
+
+productos.then((productos) => {
     productos.forEach((producto) => {
-        let {id, nombre, descripcion, amargor, volAlc, precio, moneda} = producto
-        productos.push(producto)
+        let { id, nombre, descripcion, amargor, volAlc, precio, moneda } = producto
         sectionProducts.innerHTML += `
         <div id="producto${id}" class="col">
             <div class="card p-4 m-4">
@@ -39,22 +36,125 @@ fetch("../json/productos.json")
     })
 })
 
-/*
+//Agregar productos al carrito
+productos.then(productos => {
+    productos.forEach(p => {
+        document.getElementById(`agregar${p.id}`).addEventListener("click", () => {
+            let carrito = JSON.parse(localStorage.getItem("itemCarrito")) ?? []
+            if (carrito.some(prod => prod.nombre == p.nombre)) {
+                let indice = carrito.findIndex(prod => prod.nombre == p.nombre)
+                carrito[indice].cantidad++
+                carrito[indice].precio += p.precio
+            } else {
+                let nuevoItem = {
+                    ...p,
+                    cantidad: 1
+                }
+                carrito.push(nuevoItem)
+            }
+            let carritoString = JSON.stringify(carrito)
+            localStorage.setItem("itemCarrito", carritoString)
+            Swal.fire({
+                icon: 'success',
+                iconColor: 'orange',
+                title: 'Agregado al carrito',
+                width: 600,
+                padding: '3em',
+                color: 'white',
+                background: '#fff',
+                showConfirmButton: false,
+                timer: 1000
+            })
+        })
+    });
+})
 
-//Se crea un carrito de compras
-let carrito = JSON.parse(localStorage.getItem("itemCarrito")) ?? []
+//Actualizar carrito
+function actualizarCarrito() {    
+    mostrarCarrito.innerHTML = ``
+    carrito = JSON.parse(localStorage.getItem("itemCarrito"))
+    if (carrito != null) {
+        carrito.forEach(item => {
+            mostrarCarrito.innerHTML += `
+                    <div class="card mb-3 mt-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-3 mx-auto">
+                                <img src="../assets/images/producto${item.id}.png" class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <h5 class="card-title">${item.nombre}</h5>
+                                    <p class="card-text"> ${item.moneda} ${item.precio}</p>
+                                    <p class="card-text"> Cant: ${item.cantidad}</p>
+                                    <p class="card-text"> <a id="eliminar${item.id}" class="link-warning eliminarItem" href="#"> Eliminar </a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+        })
+        carrito.forEach(item => {
+            mostrarCarrito.querySelector(`#eliminar${item.id}`).addEventListener("click", () => {
+                eliminarItem(item.id)
+            })
+        })
+    }
+    actualizarTotal()
+}
 
+//Mostrar carrito
+let mostrarCarrito = document.getElementById("mostrarCarrito")
 
-//Agregar producto al carrito
-productos.forEach(p => {
-    document.getElementById(`agregar${p.id}`).addEventListener("click", ()=> {
-        carrito.push(p)
-        let carritoString = JSON.stringify(carrito)
-        localStorage.setItem("itemCarrito", carritoString)
+document.getElementById("btn-mostrarCarrito").addEventListener("click", () => {
+    actualizarCarrito()
+})
+
+//Eliminar item del carrito
+function eliminarItem(itemId) {
+    const borrar = carrito.find((c) => c.id === itemId)
+    const indice = carrito.indexOf(borrar)
+    if (carrito[indice].cantidad > 1) {
+        let cantPrev = carrito[indice].cantidad
+        let precioPrev = carrito[indice].precio
+        carrito[indice].cantidad--
+        carrito[indice].precio = precioPrev/cantPrev * carrito[indice].cantidad
+    } else {
+        carrito.splice(indice, 1)
+    }
+    let carritoString = JSON.stringify(carrito)
+    localStorage.setItem("itemCarrito", carritoString)
+    actualizarCarrito()
+}
+
+//Actualizar importe total
+function actualizarTotal() {
+    let totalCarrito = document.getElementById("totalCarrito")
+    if (carrito == null) {
+        totalCarrito.innerHTML = `TOTAL: 0 UYU`
+    } else {
+        const total = carrito.reduce((prev, sig)=> prev + sig.precio, 0)
+        totalCarrito.innerHTML = `TOTAL: ${total} UYU`
+    }       
+}
+
+//Vaciar carrito
+function vaciarCarrito() {
+    localStorage.clear()
+    actualizarCarrito()
+}
+
+document.getElementById("btn-vaciarCarrito").addEventListener("click", () => {
+    vaciarCarrito()
+})
+
+//Finalizar compra
+document.getElementById("btn-finalizarCompra").addEventListener("click", () => {
+    carrito = JSON.parse(localStorage.getItem("itemCarrito"))
+    if (carrito == null) {
         Swal.fire({
             icon: 'success',
             iconColor: 'orange',
-            title: 'Agregado al carrito',
+            title: 'No existen productos',
             width: 600,
             padding: '3em',
             color: 'white',
@@ -62,32 +162,19 @@ productos.forEach(p => {
             showConfirmButton: false,
             timer: 1000
         })
-    })
-});
-
-//Mostrar carrito
-let mostrarCarrito = document.getElementById("mostrarCarrito")
-
-document.getElementById("btn-mostrarCarrito").addEventListener("click", ()=> {
-    carrito = JSON.parse(localStorage.getItem("itemCarrito"))
-    carrito.forEach(item => {
-        mostrarCarrito.innerHTML += `
-        <div class="card mb-3 mt-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-3 mx-auto">
-                    <img src="../assets/images/producto${item.id}.png" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-9">
-                    <div class="card-body">
-                        <h5 class="card-title">${item.nombre}</h5>
-                        <p class="card-text"> ${item.moneda} ${item.precio}</p>
-                        <p class="card-text"> <a id="eliminar${item.id}" class="link-warning" href="#"> Eliminar </a></p>
-                    </div>
-                </div>
-            </div>
-      </div>
-    `
-    })
-});
-
-*/
+    }
+    else {
+        Swal.fire({
+            icon: 'success',
+            iconColor: 'orange',
+            title: 'Gracias por tu compra',
+            width: 600,
+            padding: '3em',
+            color: 'white',
+            background: '#fff',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
+    vaciarCarrito()
+})
